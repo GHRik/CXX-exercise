122330cba2291a399826895bb0ca6d689985ed4f
Damian Panasiuk <damian.panasiuk@tieto.com>
2021-10-07 16:14:52 +0200

    Implements core of exercise-1
    Create vector of interface Animal. This vetor is type of share_ptr.
    Share_ptr was used, because unique_ptr can not be copied. Any object
    to "push_back" on vector pushing by "copied".
    
    If you want to use unique_ptr you should use std::move.
    
    Partially-Implements: Exercise-1

--- Exercise-1/main.cpp
+++ Exercise-1/main.cpp
@@ -2,14 +2,31 @@
 #include "headers/Cat.hpp"
 #include "headers/Dog.hpp"
 #include "headers/Monkey.hpp"
-
-using namespace std;
+#include "headers/Interfaces/Animal.hpp"
+#include <vector>
+#include <memory>
 
 int main()
 {
-    Cat cat;
-    Dog dog;
-    Monkey monkey;
+    /*
+    shared_ptr - can be copied and moved.
+    unique_ptr - can be moves, CANT be copied.
+    auto_ptr - old unique_ptr , not secured
+    weake_ptr - copied of shared_ptr , but he must be used with shared_ptr.
+    https://www.geeksforgeeks.org/auto_ptr-unique_ptr-shared_ptr-weak_ptr-2/
+    */
+    std::shared_ptr<Animal> cat(new Cat());
+    std::shared_ptr<Animal> dog(new Dog());
+    std::shared_ptr<Animal> monkey(new Monkey());
+
+    std::vector<std::shared_ptr<Animal>> myAnimals;
+    myAnimals.push_back(cat);
+    myAnimals.push_back(dog);
+    myAnimals.push_back(monkey);
+
+    for(unsigned int i=0; i<myAnimals.size(); ++i){
+        std::cout << myAnimals.at(i).get()->sleeping() << std::endl;
+    }
 
     return 0;
 }
